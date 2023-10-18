75d66a8ea781739e8a6a4c0813063099fad6679a
htrouble <77485440+htrouble@users.noreply.github.com>
2023-10-18 21:04:52 +0630

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+let num = 1.3453;
+
+console.log(num.toFixed(2));
+
+var name = "hello world";
+console.log(name.substr(5, 7));
+
+let date = "6/10/2023";
+console.log(date.split("/"));
+
+[day,month,year] = date.split("/");
+
+console.log(day);
+
